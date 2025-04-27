b80b9d1cb700a281b3e9874f96a972965524fb68
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2025-04-27 12:07:15 -0300

    ejercicio 2 terminado

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,4 +1,29 @@
 import './style.css'
+import { Usuario } from '../types/usuario'
 
+const usuarios: Usuario[] =[
+  {
+    id: 1,
+    nombre: "Juan",
+    edad: 25,
+    email: "juan@mail.com",
+    activo: true
+  },
+  {
+    id: 2,
+    nombre: "Ana",
+    edad: 30,
+    email: "ana@mail.com",
+    activo: false
+  },
+  {
+    id: 3,
+    nombre: "Pedro",
+    edad: 28,
+    email: "pedro@mail.com",
+    activo: true
+  }
+]
 
+console.log(usuarios.filter(usuario => usuario.activo === true))
 
